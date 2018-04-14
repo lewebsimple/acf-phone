dff1ce8ad7ec068f395da324a0b6f623da480124
Pascal Martineau <pascal@lewebsimple.ca>
2018-04-14 11:08:19 -0400

    WIP field options and hidden country / e164 sub-values

--- assets/js/acf-phone.js
+++ assets/js/acf-phone.js
@@ -5,7 +5,12 @@
    * @param $field
    */
   function initialize_field ($field) {
-    $field.find('input[type=tel]').intlTelInput({
+    let $national = $field.find('input[type=tel]'),
+      $country = $field.find('input.country'),
+      $e164 = $field.find('input.e164');
+
+    // Initialize intl-tel-input
+    $national.intlTelInput({
       utilsScript: options.utilsScriptUrl,
     });
   }
--- fields/class-acf-phone-v5.php
+++ fields/class-acf-phone-v5.php
@@ -30,25 +30,14 @@ if ( ! class_exists( 'acf_phone_field' ) ) {
 		}
 
 		/**
-		 * Render phone field input
-		 *
-		 * @param $field (array) the $field being rendered
-		 */
-		function render_field( $field ) {
-			?>
-            <input type="tel" name="<?= esc_attr( $field['name'] ) ?>" value="<?= esc_attr( $field['value'] ) ?>"/>
-            <noscript>
-                <small><?= __( "Please enable JavaScript to use this field.", 'acf-phone' ) ?></small>
-            </noscript>
-			<?php
-		}
-
-		/**
 		 * Enqueue input scripts and styles
 		 */
 		function input_admin_enqueue_scripts() {
 			$url     = $this->settings['url'];
 			$version = $this->settings['version'];
+			$options = array(
+				'utilsScriptUrl'     => "{$url}assets/js/utils.js"
+			);
 
 			wp_register_script( 'intl-tel-input', "{$url}assets/js/intlTelInput.min.js", array( 'jquery' ), '12.1.0' );
 			wp_register_script( 'acf-phone', "{$url}assets/js/acf-phone.js", array(
@@ -56,9 +45,7 @@ if ( ! class_exists( 'acf_phone_field' ) ) {
 				'intl-tel-input'
 			), $version );
 			wp_enqueue_script( 'acf-phone' );
-			wp_localize_script( 'acf-phone', 'options', array(
-				'utilsScriptUrl' => "{$url}assets/js/utils.js"
-			) );
+			wp_localize_script( 'acf-phone', 'options', $options );
 
 			wp_register_style( 'intl-tel-input', "{$url}assets/css/intlTelInput.css", array(), '12.1.0' );
 			wp_register_style( 'acf-phone', "{$url}assets/css/acf-phone.css", array(
@@ -66,6 +53,28 @@ if ( ! class_exists( 'acf_phone_field' ) ) {
 				'intl-tel-input'
 			), $version );
 			wp_enqueue_style( 'acf-phone' );
+		}
+
+		/**
+		 * Render phone field input
+		 *
+		 * @param $field (array) the $field being rendered
+		 */
+		function render_field( $field ) {
+			$name  = $field['name'];
+			$value = wp_parse_args( $field['value'], array(
+				'national' => '',
+				'country'  => 'CA',
+				'e164'     => '',
+			) );
+			?>
+            <input type="tel" name="<?= $name ?>[national]" value="<?= $value['national'] ?>"/>
+            <input type="hidden" name="<?= $name ?>[country]" value="<?= $value['country'] ?>" class="country"/>
+            <input type="hidden" name="<?= $name ?>[e164]" value="<?= $value['e164'] ?>" class="e164"/>
+            <noscript>
+                <small><?= __( "Please enable JavaScript to use this field.", 'acf-phone' ) ?></small>
+            </noscript>
+			<?php
 		}
 
 		/**
