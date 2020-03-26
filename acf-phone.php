1179bc30baada0a1d572f8eb932146f683a3d032
Pascal Martineau <pascal@lewebsimple.ca>
2020-03-26 11:19:31 -0400

    Add composer.json

--- acf-phone.php
+++ acf-phone.php
@@ -9,7 +9,7 @@
  * License URI:     http://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:     acf-phone
  * Domain Path:     /languages
- * Version:         1.0.1
+ * Version:         1.0.2
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -24,7 +24,7 @@ if ( ! class_exists( 'acf_phone_plugin' ) ) {
 
 		function __construct() {
 			$this->settings = array(
-				'version' => '1.0.1',
+				'version' => '1.0.2',
 				'url'     => plugin_dir_url( __FILE__ ),
 				'path'    => plugin_dir_path( __FILE__ )
 			);
--- readme.txt
+++ readme.txt
@@ -11,6 +11,9 @@ Phone number field for Advanced Custom Field v5.
 
 == Changelog ==
 
+= 1.0.2 =
+* Add composer.json
+
 = 1.0.1 =
 * Fix IE compatibility
 
